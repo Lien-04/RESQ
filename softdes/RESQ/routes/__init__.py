b552bce6cbf0a31abb6179a204c477b679065254
Lien-04 <neilcatilo2004@gmail.com>
2026-05-13 07:57:51 +0800

    Delete softdes directory

--- softdes/RESQ/routes/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Routes package initialization
-"""
-from .auth import auth_bp
-from .incidents import incidents_bp
-from .admin import admin_bp
-
-__all__ = ['auth_bp', 'incidents_bp', 'admin_bp']
